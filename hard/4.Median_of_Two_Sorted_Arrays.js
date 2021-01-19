adc6e0acb6cadaf03a30aee4c3c20b3f59232cb8
Noofreuuuh <dev.n2ofr@gmail.com>
2021-01-19 19:32:08 +0100

    add use case 4

---
+++ hard/4.Median_of_Two_Sorted_Arrays.js
@@ -0,0 +1,65 @@
+/*
+Title: 4. Median of Two Sorted Arrays
+URL: https://leetcode.com/problems/median-of-two-sorted-arrays/
+Personal Submission:
+    https://leetcode.com/submissions/detail/445114963/
+    2091 / 2091 test cases passed.
+    Runtime: 136 ms, faster than 72.34% of JavaScript online submissions for Median of Two Sorted Arrays.
+    Memory Usage: 45.2 MB, less than 53.91% of JavaScript online submissions for Median of Two Sorted Arrays.
+
+----
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+Follow up: The overall run time complexity should be O(log (m+n)).
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+Example 3:
+
+Input: nums1 = [0,0], nums2 = [0,0]
+Output: 0.00000
+Example 4:
+
+Input: nums1 = [], nums2 = [1]
+Output: 1.00000
+Example 5:
+
+Input: nums1 = [2], nums2 = []
+Output: 2.00000
+ 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const arr = nums1.concat(nums2).sort((a, b) => a - b);
+    if(arr.length == 1) return arr[0]
+    
+    const mid = Math.floor(arr.length / 2);
+    return arr.length % 2 !== 0 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
+};
+
+module.exports = findMedianSortedArrays;
