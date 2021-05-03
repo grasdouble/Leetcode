7a46fdf534bd5c8346455789f65637c11e43dd6e
Noofreuuuh <dev.n2ofr@gmail.com>
2021-05-04 00:06:12 +0200

    add 9.Palindrome_Number

---
+++ easy/__tests__/9.Palindrome_Number.test.js
@@ -0,0 +1,21 @@
+const isPalindrome = require('../9.Palindrome_Number');
+
+describe('reverse', () => {
+    it('test 1', () => {
+        expect(isPalindrome(121)).toEqual(true);
+    });
+
+    it('test 2', () => {
+        expect(isPalindrome(-121)).toEqual(false);
+    });
+
+    it('test 3', () => {
+        expect(isPalindrome(10)).toEqual(false);
+    });
+
+    it('test 4', () => {
+        expect(isPalindrome(-101)).toEqual(false);
+    });
+
+});
+
