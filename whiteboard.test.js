752f2aeb410359867a1a088469c741adc0adf459
Noofreuuuh <dev.n2ofr@gmail.com>
2021-05-04 21:34:25 +0200

    add whiteboard

---
+++ whiteboard.test.js
@@ -0,0 +1,15 @@
+/**
+ * Whiteboard to test code
+ */
+
+var whiteboard = function() {
+    return true;
+};
+
+
+
+describe('whiteboard', () => {
+    it('test', () => {
+        expect(whiteboard()).toEqual(true);
+    });
+});
