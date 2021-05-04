b2121bc46a2e1f4636f0f0a80828e3949c646b38
Noofreuuuh <dev.n2ofr@gmail.com>
2021-05-04 21:56:00 +0200

    remove unused test file and 127. Word Ladder waiting to have my own solution

--- 3-hard/127. Word Ladder/perso/127.Word_Ladder.test.js
+++
@@ -1,7 +0,0 @@
-const ladderLength = require('./127.Word_Ladder');
-
-describe('ladderLength', () => {
-    it('ladderLength v1', () => {
-        expect(ladderLength('hit', 'cog', ["hot","dot","dog","lot","log","cog"])).toBe(5);    
-    });
-});
